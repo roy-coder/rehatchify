435a07ddfac485f58b26ab3235804bd5f77eb6ca
Roye <roye@bitovi.com>
2024-03-29 12:57:01 -0400

    Initial commit from create-remix

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { vitePlugin as remix } from '@remix-run/dev'
+import morgan from 'morgan'
+import { remixDevTools } from 'remix-development-tools/vite'
+import { defineConfig, type ViteDevServer } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import remixConfig from './remix.config'
+
+export default defineConfig({
+  build: { manifest: true },
+  plugins: [
+    morganPlugin(),
+    remixDevTools(),
+    tsconfigPaths(),
+    remix(remixConfig),
+  ],
+})
+
+function morganPlugin() {
+  return {
+    name: 'morgan-plugin',
+    configureServer(server: ViteDevServer) {
+      return () => {
+        server.middlewares.use(morgan('tiny'))
+      }
+    },
+  }
+}
